1efc265ed121785cc1de15efb6434fb6b11c6723
cmalamug <ciara@malamug.com>
2025-04-28 11:46:47 -0400

    maybe fixed for mac -- need to install certificates for python if getting error

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from link_scraper import gen_query, google_search
+from link_scraper import generate_query, google_search
 
 def main():
    # inputs = input("Enter keywords separated by a comma: ").split(",")
    inputs = ['1 year', 'director', 'cornell business analytics']
 
-   query = gen_query(inputs)
+   query = generate_query(inputs)
    urls = google_search(query)
 
     
